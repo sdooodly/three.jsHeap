b57122a28a7c1c17fd67b7d05fa39491e9526b68
sdooodly <gayathrigs96@gmail.com>
2023-05-04 19:25:34 +0530

    3 shapes

--- three/main.js
+++ three/main.js
@@ -7,18 +7,54 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+// create dodecahedron
+const dodecahedronGeometry = new THREE.DodecahedronGeometry( 1 );
+const dodecahedronMaterial = new THREE.MeshBasicMaterial( { color: 0x21fcff} );
+const dodecahedron = new THREE.Mesh( dodecahedronGeometry, dodecahedronMaterial );
+scene.add( dodecahedron );
+
+const dodecahedronEdges = new THREE.EdgesGeometry( dodecahedronGeometry );
+const dodecahedronEdgesMaterial = new THREE.LineBasicMaterial( { color: 0x000000 } );
+const dodecahedronWireframe = new THREE.LineSegments( dodecahedronEdges, dodecahedronEdgesMaterial );
+dodecahedron.add( dodecahedronWireframe );
+
+// create icosahedron
+const icosahedronGeometry = new THREE.IcosahedronGeometry( 1 );
+const icosahedronMaterial = new THREE.MeshBasicMaterial( { color: 0xf407ff } );
+const icosahedron = new THREE.Mesh( icosahedronGeometry, icosahedronMaterial );
+icosahedron.position.x = 3;
+scene.add( icosahedron );
+
+const icosahedronEdges = new THREE.EdgesGeometry( icosahedronGeometry );
+const icosahedronEdgesMaterial = new THREE.LineBasicMaterial( { color: 0x000000 } );
+const icosahedronWireframe = new THREE.LineSegments( icosahedronEdges, icosahedronEdgesMaterial );
+icosahedron.add( icosahedronWireframe );
+
+// create tetrahedron
+const tetrahedronGeometry = new THREE.TetrahedronGeometry( 1 );
+const tetrahedronMaterial = new THREE.MeshBasicMaterial( { color: 0x0000ff } );
+const tetrahedron = new THREE.Mesh( tetrahedronGeometry, tetrahedronMaterial );
+tetrahedron.position.x = -3;
+scene.add( tetrahedron );
+
+const tetrahedronEdges = new THREE.EdgesGeometry( tetrahedronGeometry );
+const tetrahedronEdgesMaterial = new THREE.LineBasicMaterial( { color: 0x000000 } );
+const tetrahedronWireframe = new THREE.LineSegments( tetrahedronEdges, tetrahedronEdgesMaterial );
+tetrahedron.add( tetrahedronWireframe );
 
 camera.position.z = 5;
 
 function animate() {
 	requestAnimationFrame( animate );
 
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
+	dodecahedron.rotation.x += 0.01;
+	dodecahedron.rotation.y += 0.01;
+
+	icosahedron.rotation.x -= 0.01;
+	icosahedron.rotation.y -= 0.01;
+
+	tetrahedron.rotation.x += 0.01;
+	tetrahedron.rotation.y -= 0.01;
 
 	renderer.render( scene, camera );
 }
